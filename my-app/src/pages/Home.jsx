b8f3fdfa6245a158e7cba87f808f81188edc9c7a
sukphachai siriinphiban <siriinphiban_s@silpakorn.edu>
2026-09-10 05:26:50 +0000

    10movie Gallery

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,8 +1,24 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import { movies } from "../data/data";
+import MovieCard from "../components/MovieCard";
+import MovieGallery from "../components/MovieGallery";
 function Home() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
       <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
+      <h2 className="text-2xl font-semibold text-slate-700 mt-6">ภาพยนตร์แนะนำ</h2>
+      <div className="mx-auto mt-8 max-w-5xl">
+        {/* <MovieCard title="Inthe Mandalorian and Grogu" year={2026} /> */}
+        {movies.slice(0, 3).map((movie) => (
+          <MovieCard key={movie.id} title={movie.title} year={movie.year} />
+        ))}
+      </div>
+      <h2 className="text-2xl font-semibold text-slate-700 mt-6">แกลเลอรีภาพยนตร์</h2>
+      <div  className="mx-auto mt-6 max-w-md">
+        <MovieGallery />
+      </div>
     </div>
   );
 }
